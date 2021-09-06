a9ba7ff7a4eca5b9686555e1ff0427adeaede5c9
mai0809 <Maitrayee.Kar@fnf.com>
2021-09-06 14:56:07 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.Write("Enter a number:");
+            int a = (int)Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(a + "*" + i + "=" + i * a);
+                i++;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
